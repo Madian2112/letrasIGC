aafb45be331a48b72d5e65b63751b510972c054b
Madian <madian.reyes212006@gmail.com>
2025-01-19 20:47:00 -0600

    feat: se agrearon nuevas canciones

--- src/app/pages/letras/letras.component.ts
+++ src/app/pages/letras/letras.component.ts
@@ -1,19 +1,24 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  HostListener,
+  Renderer2,
+} from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { PDFDocumentProxy } from 'ng2-pdf-viewer';
 import letras from './letras.json'; // Usa importación por defecto
-import {LetrasService} from '../../services/letras.service'
 
 interface Pdf {
   title: string;
   url: string;
   version: string;
 }
-
 @Component({
   selector: 'app-letras',
   templateUrl: './letras.component.html',
-  styleUrls: ['./letras.component.css']
+  styleUrls: ['./letras.component.css'],
 })
 export class LetrasComponent implements OnInit {
   @ViewChild('pdfViewer', { static: false }) pdfViewer!: ElementRef;
@@ -31,13 +36,9 @@ export class LetrasComponent implements OnInit {
   private touchStartX: number = 0;
   private touchStartY: number = 0;
   private lastTouchDistance: number = 0;
-  private zoomCenter: { x: number, y: number } | null = null;
+  private zoomCenter: { x: number; y: number } | null = null;
 
-  constructor(private sanitizer: DomSanitizer, 
-              private renderer: Renderer2,
-              private pdfDownloadService : LetrasService) {}
-
-
+  constructor(private sanitizer: DomSanitizer, private renderer: Renderer2) {}
 
   ngOnInit() {
     this.pdfs = (letras as any).usuarios;
@@ -45,15 +46,20 @@ export class LetrasComponent implements OnInit {
   }
 
   filterPdfs() {
-    const removeAccents = (str: string) => 
-      str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-  
+    const removeAccents = (str: string) =>
+      str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
     this.filteredPdfs = this.pdfs
-      .sort((a, b) => removeAccents(a.title).localeCompare(removeAccents(b.title)))
-      .filter(pdf => removeAccents(pdf.title.toLowerCase()).includes(removeAccents(this.searchQuery.toLowerCase())));
+      .sort((a, b) =>
+        removeAccents(a.title).localeCompare(removeAccents(b.title))
+      )
+      .filter((pdf) =>
+        removeAccents(pdf.title.toLowerCase()).includes(
+          removeAccents(this.searchQuery.toLowerCase())
+        )
+      );
   }
-  
-  
+
   handleViewPdf(pdf: Pdf) {
     this.selectedPdf = pdf;
     this.pdfSrc = pdf.url;
@@ -87,7 +93,10 @@ export class LetrasComponent implements OnInit {
   onTouchStart(event: TouchEvent) {
     if (event.touches.length === 2 && this.pdfViewer) {
       event.preventDefault();
-      this.lastTouchDistance = this.getTouchDistance(event.touches[0], event.touches[1]);
+      this.lastTouchDistance = this.getTouchDistance(
+        event.touches[0],
+        event.touches[1]
+      );
       this.zoomCenter = this.getTouchCenter(event.touches[0], event.touches[1]);
     } else if (event.touches.length === 1) {
       this.touchStartX = event.touches[0].clientX;
@@ -99,7 +108,10 @@ export class LetrasComponent implements OnInit {
   onTouchMove(event: TouchEvent) {
     if (event.touches.length === 2 && this.pdfViewer) {
       event.preventDefault();
-      const currentDistance = this.getTouchDistance(event.touches[0], event.touches[1]);
+      const currentDistance = this.getTouchDistance(
+        event.touches[0],
+        event.touches[1]
+      );
       const zoomFactor = currentDistance / this.lastTouchDistance;
       this.lastTouchDistance = currentDistance;
 
@@ -120,27 +132,42 @@ export class LetrasComponent implements OnInit {
     return Math.sqrt(dx * dx + dy * dy);
   }
 
-  private getTouchCenter(touch1: Touch, touch2: Touch): { x: number, y: number } {
+  private getTouchCenter(
+    touch1: Touch,
+    touch2: Touch
+  ): { x: number; y: number } {
     return {
       x: (touch1.clientX + touch2.clientX) / 2,
-      y: (touch1.clientY + touch2.clientY) / 2
+      y: (touch1.clientY + touch2.clientY) / 2,
     };
   }
 
   private zoomToPoint(clientX: number, clientY: number, zoomFactor: number) {
     if (this.pdfViewer) {
-      const containerRect = this.pdfViewer.nativeElement.getBoundingClientRect();
+      const containerRect =
+        this.pdfViewer.nativeElement.getBoundingClientRect();
       const offsetX = (clientX - containerRect.left) / containerRect.width;
       const offsetY = (clientY - containerRect.top) / containerRect.height;
 
       const oldZoom = this.zoom;
-      this.zoom = Math.min(Math.max(this.zoom * zoomFactor, this.minZoom), this.maxZoom);
+      this.zoom = Math.min(
+        Math.max(this.zoom * zoomFactor, this.minZoom),
+        this.maxZoom
+      );
 
       if (oldZoom !== this.zoom) {
-        const scrollElement = this.pdfViewer.nativeElement.querySelector('.ng2-pdf-viewer-container');
+        const scrollElement = this.pdfViewer.nativeElement.querySelector(
+          '.ng2-pdf-viewer-container'
+        );
         if (scrollElement) {
-          const newScrollLeft = (scrollElement.scrollLeft + offsetX * containerRect.width) * (this.zoom / oldZoom) - offsetX * containerRect.width;
-          const newScrollTop = (scrollElement.scrollTop + offsetY * containerRect.height) * (this.zoom / oldZoom) - offsetY * containerRect.height;
+          const newScrollLeft =
+            (scrollElement.scrollLeft + offsetX * containerRect.width) *
+              (this.zoom / oldZoom) -
+            offsetX * containerRect.width;
+          const newScrollTop =
+            (scrollElement.scrollTop + offsetY * containerRect.height) *
+              (this.zoom / oldZoom) -
+            offsetY * containerRect.height;
 
           requestAnimationFrame(() => {
             scrollElement.scrollLeft = newScrollLeft;
@@ -153,22 +180,34 @@ export class LetrasComponent implements OnInit {
 
   zoomIn() {
     if (this.pdfViewer) {
-      const containerRect = this.pdfViewer.nativeElement.getBoundingClientRect();
-      this.zoomToPoint(containerRect.left + containerRect.width / 2, containerRect.top + containerRect.height / 2, 1.1);
+      const containerRect =
+        this.pdfViewer.nativeElement.getBoundingClientRect();
+      this.zoomToPoint(
+        containerRect.left + containerRect.width / 2,
+        containerRect.top + containerRect.height / 2,
+        1.1
+      );
     }
   }
 
   zoomOut() {
     if (this.pdfViewer) {
-      const containerRect = this.pdfViewer.nativeElement.getBoundingClientRect();
-      this.zoomToPoint(containerRect.left + containerRect.width / 2, containerRect.top + containerRect.height / 2, 0.9);
+      const containerRect =
+        this.pdfViewer.nativeElement.getBoundingClientRect();
+      this.zoomToPoint(
+        containerRect.left + containerRect.width / 2,
+        containerRect.top + containerRect.height / 2,
+        0.9
+      );
     }
   }
 
   resetZoom() {
     this.zoom = 1;
     if (this.pdfViewer) {
-      const scrollElement = this.pdfViewer.nativeElement.querySelector('.ng2-pdf-viewer-container');
+      const scrollElement = this.pdfViewer.nativeElement.querySelector(
+        '.ng2-pdf-viewer-container'
+      );
       if (scrollElement) {
         scrollElement.scrollLeft = 0;
         scrollElement.scrollTop = 0;
